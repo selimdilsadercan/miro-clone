4d463345357f4adb9dad13b72f076fb11bac4cf2
selimdilsad <selimdilsadercan@gmail.com>
2024-02-03 14:08:12 +0300

    fix: moved and renamed empty state images

--- components/empty/EmptyBoards.tsx
+++ components/empty/EmptyBoards.tsx
@@ -27,7 +27,7 @@ function EmptyBoards() {
 
   return (
     <div className="h-full flex flex-col items-center justify-center">
-      <Image src="/note.svg" height={110} width={110} alt="Empty" />
+      <Image src="/empty/empty-boards.svg" height={110} width={110} alt="Empty" />
       <h2 className="text-2xl font-semibold mt-6">Create your first board!</h2>
       <p className="text-muted-foreground textg-sm mt-2">Start by creating a board for your organization</p>
       <div className="mt-6">
--- components/empty/EmptyOrg.tsx
+++ components/empty/EmptyOrg.tsx
@@ -6,7 +6,7 @@ import Image from "next/image";
 function EmptyOrg() {
   return (
     <div className="h-full flex flex-col items-center justify-center">
-      <Image src="/elements.svg" alt="Empty" height={200} width={200} />
+      <Image src="/empty/empty-org.svg" alt="Empty" height={200} width={200} />
       <h2 className="text-2xl font-semibold mt-6">Welcome to Miro</h2>
       <p className="text-muted-foreground text-sm mt-2">Create an organization to get started</p>
       <div className="mt-6">
