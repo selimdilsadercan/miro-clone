1732bb3f8e44fc96bb24da7776ab89f3eea65337
selimdilsad <selimdilsadercan@gmail.com>
2024-02-03 21:50:12 +0300

    fix: added suspense to OrgSidebar because it uses searchParams

--- app/(dashboard)/layout.tsx
+++ app/(dashboard)/layout.tsx
@@ -1,6 +1,7 @@
 import Navbar from "@/components/nav/Navbar";
 import OrgSidebar from "@/components/nav/OrgSidebar";
 import Sidebar from "@/components/nav/Sidebar";
+import { Suspense } from "react";
 
 interface Props {
   children: React.ReactNode;
@@ -10,9 +11,13 @@ function Layout({ children }: Props) {
   return (
     <main className="h-full">
       <Sidebar />
+
       <div className="pl-[60px] h-full">
         <div className="flex gap-x-3 h-full">
-          <OrgSidebar />
+          <Suspense fallback={<div></div>}>
+            <OrgSidebar />
+          </Suspense>
+
           <div className="h-full flex-1">
             <Navbar />
             {children}
