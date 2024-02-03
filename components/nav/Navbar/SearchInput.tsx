b0132463fdf53bdcaeae5ed318c419e497ac5ab8
selimdilsad <selimdilsadercan@gmail.com>
2024-02-03 14:04:37 +0300

    fix: added onEnter to searh input

--- components/nav/Navbar/SearchInput.tsx
+++ components/nav/Navbar/SearchInput.tsx
@@ -12,20 +12,25 @@ function SearchInput() {
   const [value, setValue] = useState("");
   const debouncedValue = useDebounce(value, 500);
 
+  const onEnter = (e: React.KeyboardEvent<HTMLInputElement>) => {
+    setValue((e.target as HTMLInputElement).value);
+    const url = qs.stringifyUrl({ url: "/", query: { search: value } }, { skipEmptyString: true, skipNull: true });
+    router.push(url);
+  };
+
   const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
     setValue(e.target.value);
   };
 
   useEffect(() => {
     const url = qs.stringifyUrl({ url: "/", query: { search: debouncedValue } }, { skipEmptyString: true, skipNull: true });
-
     router.push(url);
   }, [debouncedValue, router]);
 
   return (
     <div className="w-full relative">
       <Search className="absolute top-1/2 left-3 transform -translate-y-1/2 text-muted-foreground h-4 w-4" />
-      <Input className="w-full max-w-[516px] pl-9" placeholder="Search boards" onChange={handleChange} value={value} />
+      <Input className="w-full max-w-[516px] pl-9" placeholder="Search boards" onChange={handleChange} value={value} onKeyDown={onEnter} />
     </div>
   );
 }
